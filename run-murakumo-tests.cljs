16ee384137d920c624dbab25afab8e7386889df2
Jun Kawasaki <04.feasts_minded@icloud.com>
2026-07-27 13:16:51 +0900

    test: fix the nbb runner namespace for this repo

--- run-murakumo-tests.cljs
+++ run-murakumo-tests.cljs
@@ -3,7 +3,7 @@
 ;; Run with: nbb --classpath src:test run-murakumo-tests.cljs
 (ns run-murakumo-tests
   (:require [clojure.test :as t]
-            [kawase-yui.murakumo-test]))
+            [mitate.murakumo-test]))
 
-(let [r (t/run-tests 'kawase-yui.murakumo-test)]
+(let [r (t/run-tests 'mitate.murakumo-test)]
   (.exit js/process (if (zero? (+ (:fail r) (:error r))) 0 1)))
